e1a262767bb08a41bc72688310741ef8f4f62fcf
Ilknur Eren <ilkxeren@gmail.com>
2023-09-27 10:01:08 -0400

    update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="fs-1 fw-bold">should be 1</p>
+        <p className="fs-6 ">should be 6</p>
       </header>
     </div>
   );
